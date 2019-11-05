75c18fe91111560448643f5e3d53039fd7b6b466
Kate Davis <katedavis248@yahoo.com>
2019-11-04 23:56:08 -0600

    functions

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -26,3 +26,28 @@ function clock() {
 
 };
 clock();
+
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
+
+    name = $("#nameInput").val().trim();
+    destination = $("#destInput").val().trim();
+    firstTrain = $("#firstTrainInput").val().trim();
+    frequency = $("#freqInput").val().trim();
+
+    calculateVariables();
+
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        minutesAway: minutesAway,
+        nextArrival: nextArrival,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });//closes push
+    $("#nameInput").val("");
+    $("#destInput").val("");
+    $("#firstTrainInput").val("");
+    $("#freqInput").val("");
+});//closes .onClick
