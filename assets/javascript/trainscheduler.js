a41f4ef7466432a076d07a823fd5877702c40fb7
Kate Davis <katedavis248@yahoo.com>
2019-11-04 23:54:40 -0600

    java folder added and api added

---
+++ assets/javascript/trainscheduler.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyB5m0YBJ0J4tVmQ904oq5vJK3Dke_Gq_Rk",
+    authDomain: "trainscheduler-9a283.firebaseapp.com",
+    databaseURL: "https://trainscheduler-9a283.firebaseio.com",
+    projectId: "trainscheduler-9a283",
+    storageBucket: "trainscheduler-9a283.appspot.com",
+    messagingSenderId: "186370673105"
+};
